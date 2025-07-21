cbfd673a87a276d65c500965ef4ccdb10b5fcdb7
Your Name <your_email@example.com>
2025-07-21 16:09:09 +0530

    added4 .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -228,7 +228,7 @@ export default function Page() {
                       "impact",
                       "exploration",
                       "connection",
-                    ]}
+                    ] as string[]}
                   />
                 </div>
                 <div
